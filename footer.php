b46a12e2c38494e6cfe34d50a921d46e71c275b6
granam180 <bonneville.joey@gmail.com>
2015-09-15 13:06:55 -0400

    deleting branch to fix git merge

--- footer.php
+++
@@ -1,5 +0,0 @@
-<div class="page-footer">
-    <h3>Brought to you by <a href="http://www.joeybonneville.com" target="_blank"><em>Bonneville Design</em></a> &copy;</h3>
-    </div>
-</body>
-</html>
--- header.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head>
-		<title>Your own do-while</title>
-        <link href='https://fonts.googleapis.com/css?family=Montserrat:700' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" type="text/css" href="css/main.css">
-	</head>
-	<body>
